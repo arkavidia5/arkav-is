86613b3b3714fff64d60edcb9d70adb5c98f8339
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-10-14 00:24:43 +0700

    Add login screen appearance

--- arkav-is-web/src/App.vue
+++ arkav-is-web/src/App.vue
@@ -1,71 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
+    <router-view />
   </v-app>
 </template>
 
@@ -75,18 +10,33 @@ export default {
   name: 'App',
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.jsl'
     }
   }
 }
 </script>
+
+<style>
+.application a {
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+}
+
+.application a::after{
+  content: '';
+  border-bottom: 6px solid;
+  border-bottom-color: var(--v-primary-base, #1976d2);
+  position: absolute;
+  opacity: 0.25;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+
+.application a:hover::after{
+  opacity: 0.4;
+  transition: 0.2s;
+}
+
+</style>
---
+++ arkav-is-web/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-3 pa-3">
+          <v-card-text>
+            <h1>Arkavidia 5</h1>
+            <v-form class="mt-3">
+              <v-text-field v-model="username" label="Username" required></v-text-field>
+              <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
+              <div class="my-3">
+                <a href="#">Lupa password?</a>
+              </div>
+              <v-btn depressed large block color="primary">Login</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      username: '',
+      password: '',
+    })
+  }
+</script>
+
+<style>
+  
+</style>
