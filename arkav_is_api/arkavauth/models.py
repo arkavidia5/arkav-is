d15a90d1b1e3d195d77b579d452b930c5df879fd
Yonas Adiel <yonazadielwiguna@gmail.com>
2018-11-05 12:55:52 +0700

    done beautify

--- arkav_is_api/arkavauth/models.py
+++ arkav_is_api/arkavauth/models.py
@@ -1,9 +1,8 @@
 from string import ascii_letters
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.core.mail import send_mail, EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives
 from django.db import models
 from django.template.loader import get_template
-from django.template import Context
 from django.utils.crypto import get_random_string
 from django.utils.translation import ugettext_lazy as _
 
@@ -76,14 +75,21 @@ class EmailConfirmationAttempt(models.Model):
     def send_email(self):
         link = 'https://dashboard.arkavidia.id/confirm-email/%s' % (self.token)
 
+        template = get_template('confirm_email.html')
+        context = {
+            'user': self.user,
+            'link': link
+        }
+
         mail_subject = 'Email Confirmation'
         mail_message = 'One last step to use your account!'
         mail_from = 'Arkavidia 5.0 <noreply@arkavidia.id>'
         mail_to = self.user.email
-        # TODO: make good html message
-        mail_html_message = 'Click <a href="%s"> here </a> to confirm your email' % (link)
+        mail_html_message = template.render(context)
 
-        send_mail(mail_subject, mail_message, mail_from, [mail_to], html_message=mail_html_message, fail_silently=False)
+        mail = EmailMultiAlternatives(mail_subject, mail_message, mail_from, [mail_to])
+        mail.attach_alternative(mail_html_message, "text/html")
+        mail.send()
 
     def confirm(self):
         self.confirmed = True
@@ -128,5 +134,3 @@ class PasswordResetAttempt(models.Model):
         mail = EmailMultiAlternatives(mail_subject, mail_message, mail_from, [mail_to])
         mail.attach_alternative(mail_html_message, "text/html")
         mail.send()
-
-        send_mail(mail_subject, mail_message, mail_from, [mail_to], html_message=mail_html_message, fail_silently=False)
